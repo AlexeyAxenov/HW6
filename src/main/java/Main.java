2d6a1ba1f9aac1645a8855ea8f565c40a2240f87
Axeley <linkinpark099@yandex.ru>
2022-03-30 13:13:56 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+
+
+        HttpUrl httpUrl = new HttpUrl.Builder()
+                .scheme("http")
+                .host("dataservice.accuweather.com")
+                .addPathSegment("forecasts")
+                .addPathSegment("v1")
+                .addPathSegment("daily")
+                .addPathSegment("5day")
+                .addPathSegment("332663")
+                .addQueryParameter("apikey","U2supnJ6luhCWZ7mG2N1stgz14eEguXH")
+                .build();
+
+        Request request = new Request.Builder()
+                .url(httpUrl)
+                .build();
+
+
+        Response response = okHttpClient.newCall(request).execute();
+        System.out.println(response.body().string());
+    }
+}
